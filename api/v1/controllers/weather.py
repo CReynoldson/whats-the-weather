803adc192d2778f53af0ce588449e7e22a8a8a83
Courtney Reynoldson <ctreynoldson@gmail.com>
2021-05-04 20:16:43 -0700

    add actually calling openweather, error handling

--- api/v1/controllers/weather.py
+++ api/v1/controllers/weather.py
@@ -1,19 +1,37 @@
+from app import weather_service
+from exceptions.weather import WeatherServiceInvalidParametersException
+
 class WeatherHelper():
 
     # let's make sure no one passes us anything Nefarious™
+    # latitude and longitude aren't implemented yet but seem logical additions 
     ARGUMENTS_WHITELIST = ["city", "latitude", "longitude"]
 
     def __init__(self, *args):
-        # figuring out this circular import tomorrow and yes the shame is unbearable thank you for asking
-        from services.weather import WeatherService
-        self.service = WeatherService()
         for key, value in args[0].items():
             if key in self.ARGUMENTS_WHITELIST:
                 self.__setattr__(key, value)
 
     def format_response(self, response):
-        raise NotImplementedError
+        """
+        Parses OpenWeather API response and returns only the relevant weather data
+        :response => The json results of our API call to the weather service
+        :rtype => dict
+        """
+        weather_data = {
+            "weather": response["main"]
+        }
+        return weather_data
 
     def get_weather(self):
-        service_response = self.service.get_weather_by_city_name(self.city)
+        """
+        Calls weather service to proxy API call to OpenWeather
+        This would be a great place to add additional query parameter handling if we ever wanted to
+        check the weather based on something like latitude or longitude.
+        For now, let's just have it validate that we have something to base our query on
+        :rtype => dict
+        """
+        if not self.city:
+            raise WeatherServiceInvalidParametersException("Please provide a city, we simply cannot tell you the weather in a city without a city to check")
+        service_response = weather_service.get_weather_by_city_name(self.city)
         return self.format_response(service_response)
--- api/v1/routes.py
+++ api/v1/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request
-from .controllers.weather import WeatherHelper
+from flask import Blueprint, request, jsonify
+from api.v1.controllers.weather import WeatherHelper
+from exceptions.weather import WeatherServiceException
 
 v1_api = Blueprint("v1", __name__, url_prefix="/v1")
 
@@ -9,5 +10,7 @@ def get_weather():
     city_name = request.args.get("city")
     query_parameters = request.args
     helper = WeatherHelper(query_parameters)
-    helper.get_weather()
-    return "Get ready to start singing (or scowling) in the rain"
+    try:
+        return jsonify({"results": "success", "data": helper.get_weather()}), 200
+    except WeatherServiceException as e:
+        return jsonify({"results": "failure", "message": e.args[0]}), e.status_code
