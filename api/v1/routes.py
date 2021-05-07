cc98dcf7218e57f588fb9da4cc05fc9708e0f2b1
Courtney Reynoldson <ctreynoldson@gmail.com>
2021-05-06 22:47:20 -0700

    add trite proof of concept authentication example

--- api/v1/routes.py
+++ api/v1/routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, jsonify
 from api.v1.controllers.weather import WeatherHelper
 from exceptions.weather import WeatherServiceException
-
+from utilities import authenticated
 v1_api = Blueprint("v1", __name__, url_prefix="/v1")
 
 
 @v1_api.route("/weather")
+@authenticated
 def get_weather():
     city_name = request.args.get("city")
     query_parameters = request.args
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os 
 class Config(object):
     WEATHER_SERVICE_API_KEY = os.environ["WEATHER_SERVICE_API_KEY"]
-    ENVIRONMENT = os.environ.get("ENVIRONMENT", "production")
+    ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
+    API_TOKEN = os.environ.get("API_TOKEN", "notevenalittlebitsecureatall")
