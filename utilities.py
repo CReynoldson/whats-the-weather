a2704b3e4fa8e64ace7367c3ff4803f73abc031b
Courtney Reynoldson <ctreynoldson@gmail.com>
2021-05-06 22:55:17 -0700

    rename variable for Consistency

--- utilities.py
+++ utilities.py
@@ -4,12 +4,12 @@ from app import app
 # this is a terrible proof of concept example, obviously these would be unique to users 
 # and stored in / loaded from a database 
 
-API_AUTH = app.config.get("API_TOKEN")
+API_TOKEN = app.config.get("API_TOKEN")
 
 def authenticated(f):
     def wrap(*args, **kwargs):
         token = request.args.get("app-token")
-        if token != API_AUTH:
+        if token != API_TOKEN:
             return jsonify({"results": "failure", "message": "It's a low, low bar, and you couldn't even hit it."}), 401
         return f(*args, **kwargs)
     return wrap
